9c4ebc36d1a5e097d508a9e28217ccb633514bc1
Sihwan Kim <vkn123@naver.com>
2024-08-20 04:17:32 +0900

    feat: [common] 로그인 api적용

--- shared/src/commonMain/kotlin/com/allyouraffle/allyouraffle/network/LoginApi.kt
+++ shared/src/commonMain/kotlin/com/allyouraffle/allyouraffle/network/LoginApi.kt
@@ -12,37 +12,17 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.Serializable
 
 
-@Serializable
-data class MobileUserLoginDto(
-    val registrationId : String,
-    val email : String,
-    val name : String,
-    val profileImageUrl : String?,
-    val userNameAttributeNameValue : String
-)
-@Serializable
-data class MobileLoginResponse(
-    val jwt : String,
-    val refreshToken : String
-)
-
-@Serializable
-data class JwtTokenResponse(
-    val jwt : String,
-)
-
-
 object LoginApi {
     fun login(mobileUserLoginDto: MobileUserLoginDto): MobileLoginResponse {
         return runBlocking {
             val response = ktorClient
-                .post(BASE_URL + "api/v1/login"){
+                .post(BASE_URL + "api/v1/login") {
                     contentType(ContentType.Application.Json)
                     setBody(mobileUserLoginDto)
                 }
             println(response.status)
 
-            if ( response.status != HttpStatusCode.OK){
+            if (response.status != HttpStatusCode.OK) {
                 throw NetworkException()
             }
 
@@ -52,20 +32,135 @@ object LoginApi {
 
     fun verify(jwt: String): Boolean {
         return runBlocking {
-            val response = ktorClient.post(BASE_URL + "api/v1/login/verify?jwt="+jwt)
+            val response = ktorClient.post(BASE_URL + "api/v1/login/verify?jwt=" + jwt)
             return@runBlocking response.status == HttpStatusCode.OK
 
         }
     }
 
-    fun refresh(refreshToken: String) : String? {
+    fun refresh(refreshToken: String): String? {
         return runBlocking {
-            val response = ktorClient.post(BASE_URL+"api/v1/login/refresh?refresh="+refreshToken)
+            val response =
+                ktorClient.post(BASE_URL + "api/v1/login/refresh?refresh=" + refreshToken)
             println(response.toString())
-            if (response.status != HttpStatusCode.OK){
+            if (response.status != HttpStatusCode.OK) {
                 return@runBlocking null
             }
             return@runBlocking response.body()
         }
     }
+
+    fun getUserInfo(jwt: String): UserInfoResponse {
+        return runBlocking {
+            val response = ktorClient.get(BASE_URL + "api/v1/user/mypage") {
+                headers["Authorization"] = "Bearer $jwt"
+            }
+            println(response)
+            checkNotOkThrowNetworkException(response.status)
+
+            return@runBlocking response.body()
+
+        }
+    }
+
+    fun setUserAddress(jwt : String,addressRequestDto: AddressRequestDto): Boolean {
+        return runBlocking {
+            val response = ktorClient.post(BASE_URL+"api/v1/user/set_address"){
+                headers["Authorization"] = "Bearer $jwt"
+                contentType(ContentType.Application.Json)
+                setBody(addressRequestDto)
+            }
+            println(response)
+            checkNotOkThrowNetworkException(response.status)
+            return@runBlocking true
+        }
+    }
 }
+
+
+@Serializable
+data class MobileUserLoginDto(
+    val registrationId: String,
+    val email: String,
+    val name: String,
+    val profileImageUrl: String?,
+    val userNameAttributeNameValue: String
+)
+
+@Serializable
+data class MobileLoginResponse(
+    val jwt: String,
+    val refreshToken: String
+)
+
+@Serializable
+data class JwtTokenResponse(
+    val jwt: String,
+)
+
+@Serializable
+data class UserInfoResponse(
+    val userId: String,
+    val email: String,
+    val name: String,
+    val nickname: String,
+    val password: String?,
+    val phoneNumber: String?,
+    val profileImageUrl: String?,
+    val address: Address?,
+    val role: String,
+    val id: Int,
+    val createdDate: String?,
+    val updatedDate: String?,
+    val deletedDate: String?
+)
+
+@Serializable
+data class Address(
+    val address: String,
+    val addressEnglish: String,
+    val bname: String,
+    val jibunAddress: String,
+    val jibunAddressEnglish: String,
+    val roadAddress: String,
+    val sido: String,
+    val sigungu: String,
+    val detail: String,
+    val postalCode: String,
+    val country: String,
+    val isDefault: Boolean,
+    val id: Int,
+    val createdDate: String?,
+    val updatedDate: String?,
+    val deletedDate: String?
+)
+
+@Serializable
+data class AddressRequestDto(
+    val address: String,
+    val addressEnglish: String,
+    val bname: String,
+    val jibunAddress: String,
+    val jibunAddressEnglish: String,
+    val roadAddress: String,
+    val sido: String,
+    val sigungu: String,
+    val detail: String,
+    val postalCode: String,
+    val country: String,
+    val isDefault: Boolean,
+)
+
+data class AddressInfo(
+    val address: String,
+    val addressEnglish: String,
+    val bname: String,
+    val jibunAddress: String,
+    val jibunAddressEnglish: String,
+    val roadAddress: String,
+    val sido: String,
+    val sigungu: String,
+    val postalCode: String,
+    val country: String,
+    val detail : String? = null
+)
--- shared/src/commonMain/kotlin/com/allyouraffle/allyouraffle/viewModel/AddressViewModel.kt
+++ shared/src/commonMain/kotlin/com/allyouraffle/allyouraffle/viewModel/AddressViewModel.kt
@@ -1,4 +1,50 @@
 package com.allyouraffle.allyouraffle.viewModel
 
+import com.allyouraffle.allyouraffle.network.Address
+import com.allyouraffle.allyouraffle.network.AddressInfo
+import com.allyouraffle.allyouraffle.network.AddressRequestDto
+import com.allyouraffle.allyouraffle.network.LoginApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
 class AddressViewModel {
+    private val _userAddress = MutableStateFlow<AddressInfo?>(null)
+    val userAddress = _userAddress.asStateFlow()
+
+    private val _detail = MutableStateFlow<String>("")
+    val detail = _detail.asStateFlow()
+
+    fun setDetail(data: String) {
+        _detail.update {
+            data
+        }
+    }
+
+    fun setUserAddress(address: AddressInfo) {
+        _userAddress.update {
+            address
+        }
+    }
+
+    fun saveUserAddress(jwt : String): Boolean {
+        val finalAddress = _userAddress.value
+        checkNotNull(finalAddress)
+        val addressRequest = AddressRequestDto(
+            address = finalAddress.address,
+            addressEnglish = finalAddress.addressEnglish,
+            bname = finalAddress.bname,
+            jibunAddress = finalAddress.jibunAddress,
+            jibunAddressEnglish = finalAddress.jibunAddressEnglish,
+            roadAddress = finalAddress.roadAddress,
+            sido = finalAddress.sido,
+            sigungu = finalAddress.sigungu,
+            detail = _detail.value,
+            postalCode = finalAddress.postalCode,
+            country = finalAddress.country,
+            isDefault = true
+        )
+
+        return LoginApi.setUserAddress(jwt,addressRequest)
+    }
 }
